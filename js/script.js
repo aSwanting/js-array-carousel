1224329a786dc7c3bc58f09ddf50607abac677b9
aSwanting <gab.damico@gmail.com>
2023-10-11 11:56:01 +0200

    ref: code into 2 functions, eventlistener to doc

--- js/script.js
+++ js/script.js
@@ -1,112 +1,124 @@
-// Create array with image paths
-const images = [
-    "./img/01.webp",
-    "./img/02.webp",
-    "./img/03.webp",
-    "./img/04.webp",
-    "./img/05.webp",
-]
+function loadImages() {
 
-// Add images to DOM gallery, gallery background and thumbnail
-for (i = 0; i < images.length; i++) {
+    // Create array with image paths
+    const images = [
+        "./img/01.webp",
+        "./img/02.webp",
+        "./img/03.webp",
+        "./img/04.webp",
+        "./img/05.webp",
+    ]
 
-    const carouselDOM = document.querySelector(".carousel")
-    const toolbarDOM = document.querySelector(".thumbnail-gallery")
-    const appBodyDOM = document.querySelector(".app-main")
+    // Add images to DOM gallery, gallery background and thumbnail
+    for (i = 0; i < images.length; i++) {
 
-    const newCarouselItem = document.createElement("img")
-    const newCarouselThumbnail = document.createElement("img")
-    const carouselBackground = document.createElement("img")
+        const carouselDOM = document.querySelector(".carousel")
+        const toolbarDOM = document.querySelector(".thumbnail-gallery")
+        const appBodyDOM = document.querySelector(".app-main")
 
-    newCarouselItem.className = "carousel-img"
-    newCarouselThumbnail.className = "toolbar-thumbnail"
-    carouselBackground.className = "bg-image"
+        const newCarouselItem = document.createElement("img")
+        const newCarouselThumbnail = document.createElement("img")
+        const carouselBackground = document.createElement("img")
 
-    newCarouselItem.src = images[i]
-    newCarouselThumbnail.src = images[i]
-    carouselBackground.src = images[i]
-    newCarouselThumbnail.id = i
+        newCarouselItem.className = "carousel-img"
+        newCarouselThumbnail.className = "toolbar-thumbnail"
+        carouselBackground.className = "bg-image"
 
-    carouselDOM.append(newCarouselItem)
-    toolbarDOM.append(newCarouselThumbnail)
-    appBodyDOM.append(carouselBackground)
+        newCarouselItem.src = images[i]
+        newCarouselThumbnail.src = images[i]
+        carouselBackground.src = images[i]
+        newCarouselThumbnail.id = i
 
-    if (i === 0) {
-        newCarouselItem.classList.add("active")
-        newCarouselThumbnail.classList.add("highlighted")
-        carouselBackground.classList.add("bg-active")
+        carouselDOM.append(newCarouselItem)
+        toolbarDOM.append(newCarouselThumbnail)
+        appBodyDOM.append(carouselBackground)
+
+        if (i === 0) {
+            newCarouselItem.classList.add("active")
+            newCarouselThumbnail.classList.add("highlighted")
+            carouselBackground.classList.add("bg-active")
+        }
+
     }
 
 }
 
-// Declare active image index
-const carouselImagesDOM = document.querySelectorAll(".carousel-img")
-const toolbarThumbnailDOM = document.querySelectorAll(".toolbar-thumbnail")
-const galleryBackgroundDOM = document.querySelectorAll(".bg-image")
-let activeCarouselImage = 0
+
+function carouselNavigation() {
+
+    // Declare active image index
+    const carouselImagesDOM = document.querySelectorAll(".carousel-img")
+    const toolbarThumbnailDOM = document.querySelectorAll(".toolbar-thumbnail")
+    const galleryBackgroundDOM = document.querySelectorAll(".bg-image")
+    let activeCarouselImage = 0
 
 
-// Select image from thumbnail 
-document.querySelectorAll(".toolbar-thumbnail").forEach(function (thumbnail) {
-    thumbnail.addEventListener("click", function () {
+    // Select image from thumbnail 
+    document.querySelectorAll(".toolbar-thumbnail").forEach(function (thumbnail) {
+        thumbnail.addEventListener("click", function () {
+
+            carouselImagesDOM[activeCarouselImage].classList.remove("active")
+            toolbarThumbnailDOM[activeCarouselImage].classList.remove("highlighted")
+            galleryBackgroundDOM[activeCarouselImage].classList.remove("bg-active")
+
+            activeCarouselImage = thumbnail.id
+
+            carouselImagesDOM[activeCarouselImage].classList.add("active")
+            toolbarThumbnailDOM[activeCarouselImage].classList.add("highlighted")
+            galleryBackgroundDOM[activeCarouselImage].classList.add("bg-active")
+
+        })
+    })
+
+
+    // Step backwards through images, looping back to front
+    document.getElementById("arrow-btn-left").addEventListener("click", function () {
 
         carouselImagesDOM[activeCarouselImage].classList.remove("active")
         toolbarThumbnailDOM[activeCarouselImage].classList.remove("highlighted")
         galleryBackgroundDOM[activeCarouselImage].classList.remove("bg-active")
 
-        activeCarouselImage = thumbnail.id
+        if (activeCarouselImage > 0) {
+
+            activeCarouselImage--
+
+        } else {
+
+            activeCarouselImage = carouselImagesDOM.length - 1
+
+        }
 
         carouselImagesDOM[activeCarouselImage].classList.add("active")
         toolbarThumbnailDOM[activeCarouselImage].classList.add("highlighted")
         galleryBackgroundDOM[activeCarouselImage].classList.add("bg-active")
 
     })
-})
 
 
-// Step backwards through images, looping back to front
-document.getElementById("arrow-btn-left").addEventListener("click", function () {
+    // Step forwards through images, looping front to back
+    document.getElementById("arrow-btn-right").addEventListener("click", function () {
 
-    carouselImagesDOM[activeCarouselImage].classList.remove("active")
-    toolbarThumbnailDOM[activeCarouselImage].classList.remove("highlighted")
-    galleryBackgroundDOM[activeCarouselImage].classList.remove("bg-active")
+        carouselImagesDOM[activeCarouselImage].classList.remove("active")
+        toolbarThumbnailDOM[activeCarouselImage].classList.remove("highlighted")
+        galleryBackgroundDOM[activeCarouselImage].classList.remove("bg-active")
 
-    if (activeCarouselImage > 0) {
+        if (activeCarouselImage < carouselImagesDOM.length - 1) {
 
-        activeCarouselImage--
+            activeCarouselImage++
 
-    } else {
+        } else {
 
-        activeCarouselImage = carouselImagesDOM.length - 1
+            activeCarouselImage = 0
 
-    }
+        }
 
-    carouselImagesDOM[activeCarouselImage].classList.add("active")
-    toolbarThumbnailDOM[activeCarouselImage].classList.add("highlighted")
-    galleryBackgroundDOM[activeCarouselImage].classList.add("bg-active")
+        carouselImagesDOM[activeCarouselImage].classList.add("active")
+        toolbarThumbnailDOM[activeCarouselImage].classList.add("highlighted")
+        galleryBackgroundDOM[activeCarouselImage].classList.add("bg-active")
 
-})
+    })
 
+}
 
-// Step forwards through images, looping front to back
-document.getElementById("arrow-btn-right").addEventListener("click", function () {
-
-    carouselImagesDOM[activeCarouselImage].classList.remove("active")
-    toolbarThumbnailDOM[activeCarouselImage].classList.remove("highlighted")
-    galleryBackgroundDOM[activeCarouselImage].classList.remove("bg-active")
-
-    if (activeCarouselImage < carouselImagesDOM.length - 1) {
-
-        activeCarouselImage++
-
-    } else {
-
-        activeCarouselImage = 0
-    }
-
-    carouselImagesDOM[activeCarouselImage].classList.add("active")
-    toolbarThumbnailDOM[activeCarouselImage].classList.add("highlighted")
-    galleryBackgroundDOM[activeCarouselImage].classList.add("bg-active")
-
-})
-
+document.addEventListener("load", loadImages())
+document.addEventListener("click", carouselNavigation())
